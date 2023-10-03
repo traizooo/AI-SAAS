ff565f23538e29ff67ba7a2e095dd5843508ab00
Michael <michal.owsiejew23@gmail.com>
2023-10-03 21:07:08 +0100

    First Commit

--- components/bot-avatar.tsx
+++
@@ -1,9 +0,0 @@
-import { Avatar, AvatarImage } from "./ui/avatar"
-
-export const BotAvatar = () => {
-    return (
-        <Avatar className="h-12 w-12">
-            <AvatarImage className="p-1" src="/logo.png"/>
-        </Avatar>
-    );
-};
